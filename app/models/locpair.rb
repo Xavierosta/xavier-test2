acd5ed62d65cbaaeb2cde791185e3405093c33c3
Xavier Osta <xavier.osta@gmail.com>
2016-05-24 13:31:56 -0500

    bing api

--- app/models/locpair.rb
+++ app/models/locpair.rb
@@ -2,9 +2,11 @@ require 'open-uri'
 
 class Locpair < ActiveRecord::Base
 	def duration 
+
+#GOOGLE API
+
+=begin
 		directionsUrl = 'https://maps.googleapis.com/maps/api/directions/json?origin=&destination=&key=AIzaSyCyGxlpi-xFCFcfA7H-b339zki4DbN6ocM'
-#		origin = "Toronto"
-#		destination = "Montreal"
 		origin = self.location1
 	        destination = self.location2
 		directionsUrl.insert(60, origin)
@@ -12,5 +14,17 @@ class Locpair < ActiveRecord::Base
 		directionsRequest = open(directionsUrl) { |f| f.read }
 		directions_hash = JSON.parse(directionsRequest)
 		directions_hash["routes"][0]["legs"][0]["duration"]["text"]
+=end
+
+#BING API
+
+		directionsUrl = 'http://dev.virtualearth.net/REST/V1/Routes?wp.0=&wp.1=&optmz=timeWithTraffic&key=AicBOv83B-hmgOH7TjKTJkOMg2iy22MaKqJeoBzJy8r3pkJMH9BYRVEAnNnW_yct'
+		origin = self.location1
+		destination = self.location2
+		directionsUrl.insert(48, origin)
+		directionsUrl.insert(54 + origin.length, destination)
+		directionsRequest = open(directionsUrl) { |f| f.read }
+		directions_hash = JSON.parse(directionsRequest)
+		((directions_hash["resourceSets"][0]["resources"][0]["routeLegs"][0]["travelDuration"]/3600.0).round(2)).to_s + " hours"
 	end
 end
